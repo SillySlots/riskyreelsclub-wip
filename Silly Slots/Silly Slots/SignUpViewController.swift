d4910d4527b8fee2e014a77ab5e41f0eb2ab92a7
rayvenperkins <rayvenperkins@hotmail.com>
2022-03-20 17:44:47 -0500

    added age field to sign up

--- Silly Slots/Silly Slots/SignUpViewController.swift
+++ Silly Slots/Silly Slots/SignUpViewController.swift
@@ -13,6 +13,9 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var passwordField2: UITextField!
     @IBOutlet weak var emailField: UITextField!
     
+    @IBOutlet weak var ageField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,6 +26,18 @@ class SignUpViewController: UIViewController {
     
     @IBAction func onSignUp(_ sender: Any) {
         
+        let ageString = ageField.text!
+        let ageInt = Int(ageString)!
+        
+       // let ageInt: Int? = Int(ageField.text ?? " ")
+      //  let ageInt = Int(ageField.text) ?? 0
+        
+        if(ageInt < 18) {
+            print("ERROR: you may not sign up for an account")
+            
+            //fix this so that no segue is performed if the age is below
+            
+        } else {
         let user = PFUser()
         user.username = usernameField2.text
         user.password = passwordField2.text
@@ -35,20 +50,9 @@ class SignUpViewController: UIViewController {
             } else {
                 print("Error: \(error?.localizedDescription)")
 
-        
-    }
-    
+                    }
+            }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+        }
     }
 }
