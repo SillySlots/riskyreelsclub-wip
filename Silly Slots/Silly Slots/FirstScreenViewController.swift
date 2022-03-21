bf0748f602d3432060a64ed5ea7eb7d97f53b660
rayvenperkins <rayvenperkins@hotmail.com>
2022-03-21 11:03:50 -0500

    added basic background asset

--- Silly Slots/Silly Slots/FirstScreenViewController.swift
+++ Silly Slots/Silly Slots/FirstScreenViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 class FirstScreenViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
 
         // Do any additional setup after loading the view.
     }
