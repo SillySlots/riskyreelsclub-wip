524b3ef4ba09c474919c83c2fa8f85ee84609aad
Cmmitchem <c.m.mitchem@tcu.edu>
2022-03-24 17:40:16 -0500

    added fields to try to connect the reel images

---
+++ Silly Slots/Silly Slots/ReelPickerView.swift
@@ -0,0 +1,21 @@
+//
+//  ReelPickerView.swift
+//  Silly Slots
+//
+//  Created by Caroline Mitchem on 3/24/22.
+//
+
+import UIKit
+import Parse
+
+class ReelPickerView: UIPickerView {
+    var images = [PFObject]()
+    
+    enum K {
+        static let win = "Winner!"
+        static let lose = ""
+        static let imageArray = []
+        static let rattle = rattle
+    }
+
+}
--- Silly Slots/Silly Slots/SlotMachineViewController.swift
+++ Silly Slots/Silly Slots/SlotMachineViewController.swift
@@ -22,6 +22,8 @@ class SlotMachineViewController: UIViewController, UIPickerViewDelegate, UIPicke
     var winFlagbool = false
     var reelImage = [PFObject]()
     var selectedPost: PFObject!
+    var pickerData: [String] = [String] ()
+    var countImage: Int
     
     @IBOutlet weak var spinButton: UIButton!
     
@@ -30,7 +32,15 @@ class SlotMachineViewController: UIViewController, UIPickerViewDelegate, UIPicke
     @IBOutlet weak var resultLabel: UILabel!
  
     var bounds    = CGRect.zero
-    var dataArray = [[Int](), [Int](), [Int]()]
+    var dataArray = [[PFObject](), [PFObject](), [PFObject](),  [PFObject](),  [PFObject]()]
+    var rug = (reelImage["rug"] as? [PFObject]) ?? []
+    var ape = (reelImage["ape"] as? [PFObject]) ?? []
+    var dollar = (reelImage["dollar"] as? [PFObject]) ?? []
+    var banana = (reelImage["banana"] as? [PFObject]) ?? []
+    var w = (reelImage["w"] as? [PFObject]) ?? []
+    var m = (reelImage["m"] as? [PFObject]) ?? []
+    var g = (reelImage["i"] as? [PFObject]) ?? []
+    var g = (reelImage["g"] as? [PFObject]) ?? []
 //    var winSound  = SoundManager()
 //    var rattle    = SoundManager()
 
@@ -42,22 +52,25 @@ class SlotMachineViewController: UIViewController, UIPickerViewDelegate, UIPicke
         loadData()
         setupUIAndSound()
         spinSlots()
+        
+        
     }
     
-    
+
     func loadData() {
+        countImage = 6
         for i in 0...2 {
             for _ in 0...100 {
-                dataArray[i].append(Int.random(in: 0...K.imageArray.count - 1))
+                dataArray[i].append(Int.random(in: 0..countImage.imageArray.count - 1))
             }
         }
     }
-    
+
     func setupUIAndSound() {
         // SOUND
-       
+
         spinButton.alpha = 0
-        
+
         // UI
         bounds = spinButton.bounds
         setTrim()
@@ -66,34 +79,34 @@ class SlotMachineViewController: UIViewController, UIPickerViewDelegate, UIPicke
         slotPickerView.layer.cornerRadius   = 10
         spinButton.layer.cornerRadius   = 40
     }
-    
+
     func setTrim () {
         resultLabel.layer.borderColor = UIColor.label.cgColor
         slotPickerView.layer.borderColor  = UIColor.label.cgColor
         spinButton.layer.borderColor  = UIColor.label.cgColor
-        
+
         resultLabel.layer.borderWidth = 2
         slotPickerView.layer.borderWidth  = 2
         spinButton.layer.borderWidth  = 2
     }
-    
-    
+
+
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         super.traitCollectionDidChange(previousTraitCollection)
         guard traitCollection.userInterfaceStyle != previousTraitCollection?.userInterfaceStyle else { return }
         setTrim()
     }
-    
-    
+
+
     func spinSlots() {
         for i in 0...2 {
             slotPickerView.selectRow(Int.random(in: 3...97), inComponent: i, animated: true)
         }
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
+
         UIView.animate(withDuration : 0.5,
                        delay        : 0.3,
                        options      : .curveEaseOut,
@@ -101,36 +114,36 @@ class SlotMachineViewController: UIViewController, UIPickerViewDelegate, UIPicke
                        completion   : nil)
     }
 
-    
+
     @IBAction func onSpin(_ sender: Any) {
-       
+
         spinSlots()
         checkWinOrLose()
         animateButton()
     }
-    
-   
+
+
     func checkWinOrLose() {
         let emoji0 = slotPickerView.selectedRow(inComponent: 0)
         let emoji1 = slotPickerView.selectedRow(inComponent: 1)
         let emoji2 = slotPickerView.selectedRow(inComponent: 2)
         let emoji3 = slotPickerView.selectedRow(inComponent: 3)
-        
+
 
         if (dataArray[0][emoji0] == dataArray[1][emoji1]
          && dataArray[1][emoji1] == dataArray[2][emoji2] && dataArray[2][emoji2] == dataArray[3][emoji3]) {
             resultLabel.text = K.win
-           
+
         } else {
             resultLabel.text = K.lose
         }
     }
 
-    
+
     func animateButton(){
         // animate button
         let shrinkSize = CGRect(x: bounds.origin.x, y: bounds.origin.y, width: bounds.size.width - 15, height: bounds.size.height)
-        
+
         UIView.animate(withDuration: 0.5,
                        delay: 0.0,
                        usingSpringWithDamping: 0.1,
@@ -144,11 +157,11 @@ class SlotMachineViewController: UIViewController, UIPickerViewDelegate, UIPicke
 
 // MARK:UIslotPickerViewDataSource
 extension ViewController : UIPickerViewDelegate {
-    
+
     func slotPickerView(_ slotPickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return 100
     }
-    
+
     func numberOfComponents(in slotPickerView: UIPickerView) -> Int {
         return 4
     }
@@ -156,22 +169,22 @@ extension ViewController : UIPickerViewDelegate {
 
 // MARK:UIslotPickerViewDelegate
 extension ViewController: UIPickerViewDataSource {
-    
-    
+
+
     func slotPickerView(_ slotPickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
         return 83.5
     }
-    
-    
+
+
     func slotPickerView(_ slotPickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
         return 120.0
     }
-    
-    
+
+
     func slotPickerView(_ slotPickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
-        
+
         let pickerLabel = UILabel()
-        
+
         switch component {
             case 0 : pickerLabel.text = K.imageArray[(Int)(dataArray[0][row])]
             case 1 : pickerLabel.text = K.imageArray[(Int)(dataArray[1][row])]
@@ -179,7 +192,7 @@ extension ViewController: UIPickerViewDataSource {
             case 3 : pickerLabel.text = K.imageArray[(Int)(dataArray[3][row])]
             default : print("done")
         }
-        
+
         pickerLabel.font          = UIFont(name : K.emojiFont, size : 75)
         pickerLabel.textAlignment = NSTextAlignment.center
         return pickerLabel
