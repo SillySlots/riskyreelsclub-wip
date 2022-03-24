7f082b46e75d30eb9551d833087efce72057da05
rayvenperkins <rayvenperkins@hotmail.com>
2022-03-23 19:56:16 -0500

    created calls for ethereum log in

--- Silly Slots/Silly Slots/WalletViewController.swift
+++ Silly Slots/Silly Slots/WalletViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 import EtherWalletKit
 
 class WalletViewController: UIViewController {
+    
+    @IBOutlet weak var ethPasswordField: UITextField!
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +22,26 @@ class WalletViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func ethSignIn(_ sender: Any) {
+        // Generate a new account with its new password.
+        let password = ethPasswordField.text!
+        try? EtherWallet.account.generateAccount(password: password)
 
+        
+    }
+    
+    @IBAction func ethSignUp(_ sender: Any) {
+        
+        // Import an existing account from its private key and set its new password.
+        
+        let password = ethPasswordField.text!
+        //try? EtherWallet.account.importAccount(privateKey: "1dcbc1d6e0a4587a3a9095984cf051a1bc6ed975f15380a0ac97f01c0c045062, password: password)
+            
+            
+            
+    }
+    
+    
     /*
     // MARK: - Navigation
 
