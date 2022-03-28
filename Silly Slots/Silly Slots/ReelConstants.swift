5590a90ca63f56c79cac77712e39ab3ada9d6528
Cmmitchem <c.m.mitchem@tcu.edu>
2022-03-28 12:56:22 -0500

    Made slot machine run without images

---
+++ Silly Slots/Silly Slots/ReelConstants.swift
@@ -0,0 +1,16 @@
+//
+//  ReelConstants.swift
+//  Silly Slots
+//
+//  Created by Caroline Mitchem on 3/28/22.
+//
+
+import Foundation
+enum K {
+    static let emojiFont  = "Apple Color Emoji"
+    static let win        = "Winner!"
+    static let lose       = "3 In A Row"
+    static let sound      = "Slots"
+    static let imageArray = ["🍋", "❤️", "🍒", "🍌", "🍊", "🔔", "🍉", "👑"]
+    static let rattle     = "Rattle"
+}
--- Silly Slots/Silly Slots/ReelPickerView.swift
+++ Silly Slots/Silly Slots/ReelPickerView.swift
@@ -1,21 +1,24 @@
-////
-////  ReelPickerView.swift
-////  Silly Slots
-////
-////  Created by Caroline Mitchem on 3/24/22.
-////
 //
-//import UIKit
-//import Parse
+//  ReelPickerView.swift
+//  Silly Slots
 //
-//class ReelPickerView: UIPickerView {
-//    var images = [PFObject]()
-//    
-//    enum K {
-//        static let win = "Winner!"
-//        static let lose = ""
-//        static let imageArray = []
-//        static let rattle = rattle
-//    }
+//  Created by Caroline Mitchem on 3/24/22.
 //
-//}
+
+import UIKit
+import Parse
+import Foundation
+
+class ReelPickerView: UIPickerView {
+    var images = [PFObject]()
+    
+    enum K {
+        static let emojiFont  = "Apple Color Emoji"
+        static let win        = "Winner!"
+        static let lose       = "3 In A Row"
+        static let sound      = "Slots"
+        static let imageArray = ["🍋", "❤️", "🍒", "🍌", "🍊", "🔔", "🍉", "👑"]
+        static let rattle     = "Rattle"
+    }
+
+}
