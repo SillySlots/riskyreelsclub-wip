aa561ef25684cce31261d32b6157cc71b98f5f9b
rayvenperkins <rayvenperkins@hotmail.com>
2022-03-28 08:33:09 -0500

    updates with wallet viewcontroller and main storyboard

--- Silly Slots/Silly Slots/WalletViewController.swift
+++ Silly Slots/Silly Slots/WalletViewController.swift
@@ -17,6 +17,8 @@ class WalletViewController: UIViewController {
     
     @IBOutlet weak var confirmPasswordField: UITextField!
     
+    @IBOutlet weak var privKeyField: UITextField!
+    
     struct Wallet {
         let address: String
         let data: Data
@@ -47,6 +49,52 @@ class WalletViewController: UIViewController {
         
        let  firstPassword = firstPasswordField.text!
        let confirmPassword = confirmPasswordField.text!
+        let key = privKeyField.text!
+        
+        if(firstPassword == confirmPassword) {
+            let password = firstPassword
+            
+          //import account with private key
+        
+//            let key = "L2HRewdY7SSpB2jjKq6mwLes86umkWBuUSPZWE35Q8Pbbr8wVyss124sf124dfsf" // Some private key
+            let formattedKey = key.trimmingCharacters(in: .whitespacesAndNewlines)
+            let dataKey = Data.fromHex(formattedKey)!
+            let keystore = try! EthereumKeystoreV3(privateKey: dataKey, password: password)!
+            let name = "New Wallet"
+            let keyData = try! JSONEncoder().encode(keystore.keystoreParams)
+            let address = keystore.addresses!.first!.address
+            let wallet = Wallet(address: address, data: keyData, name: name, isHD: false)
+            
+            
+            
+            //get keystore manager from wallet data
+            let data = wallet.data
+            let keystoreManager: KeystoreManager
+            if wallet.isHD {
+                let keystore = BIP32Keystore(data)!
+                keystoreManager = KeystoreManager([keystore])
+            } else {
+                let keystore = EthereumKeystoreV3(data)!
+                keystoreManager = KeystoreManager([keystore])
+            }
+            
+            //get private key. probably a feature to add on the actual wallet
+            let ethereumAddress = EthereumAddress(wallet.address)!
+            let pkData = try! keystoreManager.UNSAFE_getPrivateKeyData(password: password, account: ethereumAddress).toHexString()
+        }
+        
+        
+        
+        
+    }
+    
+    @IBAction func ethSignUp(_ sender: Any) {
+    
+        let  firstPassword = firstPasswordField.text!
+        let confirmPassword = confirmPasswordField.text!
+        let key = privKeyField.text!
+        // Import an existing account from its private key and set its new password.
+        
         
         if(firstPassword == confirmPassword) {
             let password = firstPassword
@@ -58,36 +106,12 @@ class WalletViewController: UIViewController {
             let address = keystore.addresses!.first!.address
             let wallet = Wallet(address: address, data: keyData, name: name, isHD: false)
             
-                
-//            let keystore = try EthereumKeystoreV3.init(password: password)
-//
-//                self.performSegue(withIdentifier: "actualWalletSegue", sender: nil)
-//             } catch {
-//            // print(error.localizedDescription)
-//
-//                 print("something went wrong")
-//             }
             
-            
-    
-        // Generate a new account with its new password.
-
-        }
-    }
-    
-    @IBAction func ethSignUp(_ sender: Any) {
-    
-        let  firstPassword = firstPasswordField.text!
-        let confirmPassword = confirmPasswordField.text!
-        // Import an existing account from its private key and set its new password.
-        
-        
-        if(firstPassword == confirmPassword) {
-            let password = firstPassword
-            
-        
     
         }
+        
+        
+        
             
     }
     
